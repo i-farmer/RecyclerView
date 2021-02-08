7e8ff674ea903a0f3a969a4409dc69c87fd33741
i-farmer <ybbaijat2160@sina.com>
2021-02-08 13:53:19 +0800

    更新 LinearSpacingDecoration

--- library/src/main/java/i/farmer/widget/recyclerview/decoration/LinearSpacingDecoration.java
+++ library/src/main/java/i/farmer/widget/recyclerview/decoration/LinearSpacingDecoration.java
@@ -13,8 +13,26 @@ import androidx.recyclerview.widget.RecyclerView;
  * @description 间距
  */
 public class LinearSpacingDecoration extends RecyclerView.ItemDecoration {
+    private boolean includeEdge;
     private int itemSpacing;            // 每个item之间的间距
     private int paddingLeft, paddingTop, paddingRight, paddingBottom;           // 整个tab的padding
+
+    /**
+     * @param itemSpacing item间距
+     * @param padding     整个RecyclerView的padding，以下以此类推
+     */
+    public LinearSpacingDecoration(int itemSpacing, int padding) {
+        this(false, itemSpacing, padding);
+    }
+
+    /**
+     * @param includeEdge 是否包括边缘(使用padding实现)，true=边缘在视图内 false=边缘在视图外
+     * @param itemSpacing
+     * @param padding
+     */
+    public LinearSpacingDecoration(boolean includeEdge, int itemSpacing, int padding) {
+        this(includeEdge, itemSpacing, padding, padding, padding, padding);
+    }
 
     /**
      * @param itemSpacing   item间距
@@ -24,6 +42,20 @@ public class LinearSpacingDecoration extends RecyclerView.ItemDecoration {
      * @param paddingBottom
      */
     public LinearSpacingDecoration(int itemSpacing, int paddingLeft, int paddingTop, int paddingRight, int paddingBottom) {
+        this(false, itemSpacing, paddingLeft, paddingTop, paddingRight, paddingBottom);
+    }
+
+
+    /**
+     * @param includeEdge   是否包括边缘(使用padding实现)，true=边缘在视图内 false=边缘在视图外
+     * @param itemSpacing   item间距
+     * @param paddingLeft   整个RecyclerView的paddingLeft，以下以此类推
+     * @param paddingTop
+     * @param paddingRight
+     * @param paddingBottom
+     */
+    public LinearSpacingDecoration(boolean includeEdge, int itemSpacing, int paddingLeft, int paddingTop, int paddingRight, int paddingBottom) {
+        this.includeEdge = includeEdge;
         this.itemSpacing = itemSpacing;
         this.paddingLeft = paddingLeft;
         this.paddingTop = paddingTop;
@@ -42,18 +74,33 @@ public class LinearSpacingDecoration extends RecyclerView.ItemDecoration {
         int position = parent.getChildAdapterPosition(view);
         LinearLayoutManager layoutManager = (LinearLayoutManager) parent.getLayoutManager();
 
-        if (layoutManager.getOrientation() == LinearLayoutManager.HORIZONTAL) {
-            // 横向
-            outRect.left = position == 0 ? paddingLeft : 0;
-            outRect.top = paddingTop;
-            outRect.right = position == totalCount - 1 ? paddingRight : itemSpacing;
-            outRect.bottom = paddingBottom;
+        if (includeEdge) {
+            // 使用padding
+            if (layoutManager.getOrientation() == LinearLayoutManager.HORIZONTAL) {
+                // 横向
+                outRect.left = position == 0 ? paddingLeft / 2 : 0;
+                outRect.right = position == totalCount - 1 ? paddingRight / 2 : 0;
+                view.setPadding(paddingLeft / 2, paddingTop, paddingRight / 2, paddingBottom);
+            } else {
+                // 竖向
+                outRect.top = position == 0 ? paddingTop / 2 : 0;
+                outRect.bottom = position == totalCount - 1 ? paddingBottom / 2 : 0;
+                view.setPadding(paddingLeft, paddingTop / 2, paddingRight, paddingBottom / 2);
+            }
         } else {
-            // 竖向
-            outRect.left = paddingLeft;
-            outRect.top = position == 0 ? paddingTop : 0;
-            outRect.right = paddingRight;
-            outRect.bottom = position == totalCount - 1 ? paddingBottom : itemSpacing;
+            if (layoutManager.getOrientation() == LinearLayoutManager.HORIZONTAL) {
+                // 横向
+                outRect.left = position == 0 ? paddingLeft : 0;
+                outRect.top = paddingTop;
+                outRect.right = position == totalCount - 1 ? paddingRight : itemSpacing;
+                outRect.bottom = paddingBottom;
+            } else {
+                // 竖向
+                outRect.left = paddingLeft;
+                outRect.top = position == 0 ? paddingTop : 0;
+                outRect.right = paddingRight;
+                outRect.bottom = position == totalCount - 1 ? paddingBottom : itemSpacing;
+            }
         }
     }
 }
